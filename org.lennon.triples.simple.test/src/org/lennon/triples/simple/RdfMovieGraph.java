91725786135199cd7bee2f0719de8870b2ab4e55
jmlennon <jmlennon@gmail.com>
2013-07-19 12:52:00 -0400

    Now is the time

--- org.lennon.triples.simple.test/src/org/lennon/triples/simple/RdfMovieGraph.java
+++ org.lennon.triples.simple.test/src/org/lennon/triples/simple/RdfMovieGraph.java
@@ -1,5 +1,9 @@
 package org.lennon.triples.simple;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 public class RdfMovieGraph extends RdfGraph {
 
@@ -11,6 +15,7 @@ public class RdfMovieGraph extends RdfGraph {
 	private static final String BLADE_RUNNER_ID = "blade-runner";
 	private static final String AFRICAN_QUEEN_ID = "african-queen";
 	private static final String ALIEN_ID = "alien";
+	private static final String RELEASE_DATE_PREDICATE = "release-date";
 
 	static private RdfMovieGraph instance;
 
@@ -37,12 +42,15 @@ public class RdfMovieGraph extends RdfGraph {
 
 		add(new RdfTriple(bladeRunner, TITLE_PREDICATE, BLADE_RUNNER_TITLE));
 		add(new RdfTriple(bladeRunner, DIRECTED_BY_PREDICATE, ridleyScott));
+		add(new RdfTriple(bladeRunner, RELEASE_DATE_PREDICATE, stringToDate("June 25, 1972")));
 		
 		add(new RdfTriple(africanQueen, TITLE_PREDICATE, AFRICAN_QUEEN_ID));
 		add(new RdfTriple(africanQueen, DIRECTED_BY_PREDICATE, johnHuston));
+		add(new RdfTriple(bladeRunner, RELEASE_DATE_PREDICATE, stringToDate("February 20, 1952")));
 		
 		add(new RdfTriple(alien, TITLE_PREDICATE, ALIEN_ID));
 		add(new RdfTriple(alien, DIRECTED_BY_PREDICATE, ridleyScott));
+		add(new RdfTriple(bladeRunner, RELEASE_DATE_PREDICATE, stringToDate("June 22, 1979")));
 
 		// Question: "directed by" and "directed" are logically redundant.
 		// Do we have to represent them explicitly? I guess so - I suspect
@@ -50,6 +58,14 @@ public class RdfMovieGraph extends RdfGraph {
 		// types of triples are commutative.
 	}
 
+
+	private Date stringToDate(String dateStr) {
+		try {
+			return new SimpleDateFormat("MMMM dd, yyyy").parse(dateStr);
+		} catch (ParseException e) {
+			throw new IllegalArgumentException(e);
+		}
+	}
 
 	RdfEntity getBladeRunnerMovie() {
 		return bladeRunner;
