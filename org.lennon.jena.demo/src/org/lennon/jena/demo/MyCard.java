a8df10edccebfe3c49f1abfa5062cac36d7899d5
jmlennon <jmlennon@gmail.com>
2013-08-05 19:09:21 -0400

    More jena demo

--- org.lennon.jena.demo/src/org/lennon/jena/demo/MyCard.java
+++ org.lennon.jena.demo/src/org/lennon/jena/demo/MyCard.java
@@ -17,17 +17,23 @@ public class MyCard {
 	private static final String MATT_FULL_NAME = "Matt Lennon";
 	private static final String MATT_GIVEN_NAME = "Matthew";
 	private static final String MATT_FAMILY_NAME = "Lennon";
+	private static final String JMATTHEW_FULL_NAME = "James Matthew Lennon";
 
 	public static void main(String[] argv) {
 		Model model = ModelFactory.createDefaultModel();
 		Resource matt = model.createResource(MATT_URI)
 				.addProperty(VCARD.FN, MATT_FULL_NAME)
+				.addProperty(VCARD.FN, JMATTHEW_FULL_NAME)
 				.addProperty(VCARD.N, model.createResource()
 						.addProperty(VCARD.Given, MATT_GIVEN_NAME)
 						.addProperty(VCARD.Family, MATT_FAMILY_NAME));
 		printResource(matt, "");
 		System.out.println("===========");
 		printModel(model);
+
+		Resource matt2 = model.getResource(MATT_URI);
+		assert(matt.equals(matt2));
+		System.out.println(matt2.getPropertyResourceValue(VCARD.N).getProperty(VCARD.Given).getLiteral());
 	}
 
 	private static void printModel(Model m) {
